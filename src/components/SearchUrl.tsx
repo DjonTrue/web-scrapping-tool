c675c61f40971f62f5f0221b79dcfe6ffcc70280
Evgenii Latishev <evgeniy.latishev@yandex.ru>
2021-07-22 00:54:35 +0400

    add search-box layout

---
+++ src/components/SearchUrl.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+
+const SearchUrl: React.FC = () => {
+  return (
+    <div className="search-box">
+      <input type="text" className="search-box__input" />
+      <input type="submit" className="search-box__submit" value="Add link here" />
+    </div>
+  )
+}
+
+export default SearchUrl
